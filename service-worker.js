870d16596fcaba99e8e63542d7d6b149148fe75d
KyleAtkinsonJr <kylesjatkinson@hotmail.co.uk>
2025-07-24 21:01:53 +0100

    changes made

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,34 @@
-self.addEventListener('install', event => {
+// service-worker.js
+const CACHE_NAME = 'fitness-tracker-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.development.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+  // Add other assets your app needs offline
+];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('fitness-cache-v1').then(cache => {
-      return cache.addAll([
-        './index.html',
-        './manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
