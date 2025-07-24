054a4502a9911cd2ab92b84bc0872a5251765e2b
KyleAtkinsonJr <kylesjatkinson@hotmail.co.uk>
2025-07-24 20:51:18 +0100

    updates made

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,9 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('fitness-tracker-cache-v1').then(cache => {
+    caches.open('fitness-cache-v1').then(cache => {
       return cache.addAll([
         './index.html',
-        './manifest.json',
-        './icons/icon-192.png'
+        './manifest.json'
       ]);
     })
   );
@@ -12,8 +11,8 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
     })
   );
 });
